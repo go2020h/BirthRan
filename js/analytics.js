8033ed32301841588f7101a58e8dee837aebcba2
go2020h <go2020.hidane.inc@gmail.com>
2025-04-02 18:19:21 +0900

    update

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,4 @@
-// Google Analytics 4のタグ
+// Google Analytics 4とMicrosoft Clarityのタグ
 (function() {
   // Google tag (gtag.js)
   var analyticsScript = document.createElement('script');
@@ -13,4 +13,11 @@
 
   // グローバル関数として定義
   window.gtag = gtag;
+  
+  // Microsoft Clarity
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "qxs616s8op");
 })();
